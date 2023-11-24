ce81c8741398f4899120e388ebdb965f7f5dc977
Nathanim1919 <nathanim2tadele@gmail.com>
2023-11-24 17:39:22 +0300

    create a services page and add styled component

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
-import blogImage1 from '../../src/assets/blog/b1.jpg';
-import blogImage2 from '../../src/assets/blog/b2.jpg';
-import blogImage3 from '../../src/assets/blog/b3.jpg';
-import blogImage4 from '../../src/assets/blog/b4.jpg';
-import blogImage5 from '../../src/assets/blog/b5.jpg';
-import blogImage6 from '../../src/assets/blog/b6.jpg';
+import serviceImage1 from '../../src/assets/service/s1.jpg';
+import serviceImage2 from '../../src/assets/service/s2.jpg';
+import serviceImage3 from '../../src/assets/service/s3.jpg';
+import serviceImage4 from '../../src/assets/service/s4.jpg';
+import serviceImage5 from '../../src/assets/service/s5.jpg';
+import serviceImage6 from '../../src/assets/service/s6.jpg';
+
+
+const Container = styled.div`
+    display:grid;
+    place-items:center;
+    width: 70vw;
+    margin:3rem auto;
+`
 
 const AboutContainer = styled.div`
     display: grid;
     gap: 0rem;
     place-items: center;
-    width: 80vw;
-    margin: auto;
 
     >div:nth-child(1),
     div:nth-child(3),
@@ -24,6 +30,31 @@ const AboutContainer = styled.div`
       display: flex;
       justify-content: space-around;
       align-items: center;
+      
+      .textContent{
+        position:relative;
+
+        >*{
+          position:relative;
+          z-index:2;
+        }
+
+        h2{
+          color:#fff;
+        }
+
+        &::after{
+          position:absolute;
+          top:2%;
+          left:0;
+          width:70%;
+          height:40%;
+          background-color:#333;
+          transform:rotate(3deg);
+          content:'';
+          border-radius:10px;
+        }
+      }
 
       >*{
         flex: 1;
@@ -39,45 +70,46 @@ const AboutContainer = styled.div`
 const aboutSections = [
   {
     id: 1,
-    image: blogImage1,
+    image:serviceImage6,
     title: 'Track Your Expenses',
     description: 'Easily monitor and categorize your expenses to gain insights into your spending habits.',
   },
   {
     id: 2,
-    image: blogImage2,
+    image:serviceImage2,
     title: 'Set and Achieve Goals',
     description: 'Define financial goals, track your progress, and celebrate your achievements along the way.',
   },
   {
     id: 3,
-    image: blogImage3,
+    image:serviceImage3,
     title: 'Budgeting Made Simple',
     description: 'Effortlessly create budgets, allocate funds, and stay on top of your financial targets.',
   },
   {
     id: 4,
-    image: blogImage4,
+    image:serviceImage4,
     title: 'Secure Your Future',
     description: 'Explore investment options, retirement planning, and strategies for long-term financial security.',
   },
   {
     id: 5,
-    image: blogImage5,
+    image:serviceImage5,
     title: 'Financial Education',
     description: 'Access a wealth of financial resources, tips, and articles to enhance your financial literacy.',
   },
-  {
-    id: 6,
-    image: blogImage6,
-    title: 'User-Friendly Interface',
-    description: 'Enjoy a sleek and intuitive user interface designed for a seamless financial management experience.',
-  },
+{
+  id: 6,
+  image: serviceImage1,
+  title: 'AI powered Assistance',
+  description: 'Experience the power of artificial intelligence in financial management with our sleek and intuitive user interface designed for a seamless experience.',
+},
+
 ];
 
 function Service() {
   return (
-    <>
+    <Container>
     <div className='header'> 
         <h1>Discover Our Features</h1>
         <p>Explore the key features that make our app stand out in helping you achieve financial success.</p>
@@ -88,14 +120,14 @@ function Service() {
           <div>
           <img src={section.image} alt={`About section - ${section.title}`} />
           </div>
-          <div>
+          <div className="textContent">
             <h2>{section.title}</h2>
             <p>{section.description}</p>
           </div>
         </div>
       ))}
     </AboutContainer>
-    </>
+    </Container>
   );
 }
 
