8d2b9cd75558cea56f19dfead3939988c3c7e9a0
Nathanim1919 <nathanim2tadele@gmail.com>
2024-02-15 09:03:51 -0800

    clean all code and start fresh

--- server/config/config.js
+++
@@ -1,7 +0,0 @@
-const dotenv = require('dotenv').config();
-
-module.exports = {
-    endpoint:process.env.API_URL,
-    masterkey:process.env.API_KEY,
-    port:process.env.PORT
-}
--- server/routes/messageRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {createUserMessage,getMessages} = require('../controllers/messageController');
-
-router.post('/sendMessage', createUserMessage);
-router.get('/getMessage', getMessages);
-
-
-module.exports = router;
