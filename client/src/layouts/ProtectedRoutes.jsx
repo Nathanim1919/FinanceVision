f9ade3b24c229d571f0c15c5e9715885e25cdff9
Nathanim1919 <nathanim2tadele@gmail.com>
2024-03-09 13:48:56 -0800

    fix cors configuration

--- client/src/layouts/ProtectedRoutes.jsx
+++ client/src/layouts/ProtectedRoutes.jsx
@@ -25,7 +25,8 @@ export function ProtectedRoutes({ children }) {
 
       try {
         const response = await axios.get(`${BASE_URL}/api/v1/auth/getUser`, { withCredentials: true });
-
+        console.log('...................................')
+        console.log(response.data)
         if (response.data.data !== null) {
           dispatch(setUser(response.data.data));
         } else {
