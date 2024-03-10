0fa55a7420de886b5bc5b4854d9b495f66ccba92
Nathanim1919 <nathanim2tadele@gmail.com>
2024-03-10 01:46:40 -0800

    tried to console log the req for debugging

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -36,7 +36,7 @@ export const Login = () => {
 
       if (accessToken  && response.data.data !== null) {
         // document.cookie = `accessToken=${accessToken}`;
-        document.cookie = `accessToken=${accessToken}; path=/; domain=${VERCEL_DOMAIN};`;
+        document.cookie = `accessToken=${accessToken}; domain=.finance-vision.vercel.app; path=/;`;
 
         navigate('/dashboard', { replace: true });
 
