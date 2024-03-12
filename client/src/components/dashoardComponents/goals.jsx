ed311f745e515dc1e448d1303f5074daff13eea3
Nathanim1919 <nathanim2tadele@gmail.com>
2024-03-12 04:28:59 -0700

    populate goals when fetching user infos

--- client/src/components/dashoardComponents/goals.jsx
+++ client/src/components/dashoardComponents/goals.jsx
@@ -18,6 +18,9 @@ export const Goals = () => {
   const goals = (useSelector(selectGoals)).slice(0,3);
   const loading = useSelector(selectLoading);
 
+  useEffect(() => {
+    dispatch(fetchGoals(user._id));
+  }, [dispatch, user]);
 
   return (
     loading?<Loader/>:
--- client/src/pages/Goals/Goals.jsx
+++ client/src/pages/Goals/Goals.jsx
@@ -25,6 +25,9 @@ export const Goals = () => {
   const dispatch = useDispatch();
   const user = useSelector(state => state.auth.user);
 
+  // useEffect(() => {
+  //   dispatch(fetchGoals(user._id));
+  // }, [user]);
 
   const handleGoalSelect = (goal) => {
     setSelectGoal(goal);
