df7128a55a5f440bcebb01ddd16d642f8c506cb7
Nathanim1919 <nathanim2tadele@gmail.com>
2024-03-17 00:12:07 -0700

    fix insuffiecient resource error at the time of socket connection

--- client/src/utils/socketConnection.jsx
+++ client/src/utils/socketConnection.jsx
@@ -5,7 +5,7 @@ import io from 'socket.io-client';
 export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
-    const socket = io('http://localhost:3000');
+    const socket = io('https://financevision-2.onrender.com');
   
     return (
       <SocketContext.Provider value={socket}>
