fa32d73f47bbe0a8d579c04013a88a9fa534025a
Nathanim Tadele <nathanimt@act.com.et>
2024-07-31 13:23:46 +0300

    chnage base url

--- client/src/utils/Api.jsx
+++ client/src/utils/Api.jsx
@@ -1,5 +1,5 @@
 // export const BASE_URL = 'https://finance-vision-7yui.vercel.app/';
-export const BASE_URL='https://api.nathanimt.me';
+export const BASE_URL='https://www.api.nathanimt.me';
 // export const BASE_URL = 'https://financevision-2.onrender.com';
 
 export const VERCEL_DOMAIN = 'https://finance-vision.vercel.app';
