bde8e94fa583d4fbb5fc0dfc498471a061488b62
Nathanim Tadele <nathanimt@act.com.et>
2024-07-31 16:04:40 +0300

    change backend subdomain

--- client/src/utils/Api.jsx
+++ client/src/utils/Api.jsx
@@ -1,4 +1,4 @@
-export const BASE_URL='https://www.api.nathanimt.me';
+export const BASE_URL='https://api.nathanimt.me';
 export const VERCEL_DOMAIN = 'https://finance-vision.vercel.app';
 export const SOCKET_URL = 'https://finance-vision.vercel.app';
 export const GOOGLE_API_KEY="AIzaSyC_Q1Dv_cPfOdZ6IQqmpJk13DbnZ1LvxAc";
